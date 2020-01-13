0aea9fedfdef6da5e9dd8803e6f67b9a392ce998
Tiago Pimentel <tiagopms@gmail.com>
2020-01-13 16:22:42 +0000

    Save demo files in different folder

--- demo.sh
+++ demo.sh
@@ -3,28 +3,29 @@ set -e
 
 # Makes programs, downloads sample data, trains a GloVe model, and then evaluates it.
 # One optional argument can specify the language used for eval script: matlab, octave or [default] python
-
+RESULTS_DIR=results/
+CORPUS=${RESULTS_DIR}text8
+# echo $CORPUS
 make
-if [ ! -e text8 ]; then
+if [ ! -e ${CORPUS} ]; then
   if hash wget 2>/dev/null; then
-    wget http://mattmahoney.net/dc/text8.zip
+    wget -P ${RESULTS_DIR} http://mattmahoney.net/dc/text8.zip
   else
     curl -O http://mattmahoney.net/dc/text8.zip
   fi
-  unzip text8.zip
-  rm text8.zip
+  unzip ${RESULTS_DIR}text8.zip -d ${RESULTS_DIR}
+  rm ${RESULTS_DIR}text8.zip
 fi
 
-CORPUS=text8
-VOCAB_FILE=vocab.txt
-COOCCURRENCE_FILE=cooccurrence.bin
-COOCCURRENCE_SHUF_FILE=cooccurrence.shuf.bin
+VOCAB_FILE=${RESULTS_DIR}vocab.txt
+COOCCURRENCE_FILE=${RESULTS_DIR}cooccurrence.bin
+COOCCURRENCE_SHUF_FILE=${RESULTS_DIR}cooccurrence.shuf.bin
 BUILDDIR=build
-SAVE_FILE=vectors
+SAVE_FILE=${RESULTS_DIR}vectors
 VERBOSE=2
 MEMORY=4.0
 VOCAB_MIN_COUNT=5
-VECTOR_SIZE=50
+VECTOR_SIZE=100
 MAX_ITER=15
 WINDOW_SIZE=15
 BINARY=2
@@ -42,7 +43,7 @@ echo "$ $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file 
 $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE
 if [ "$CORPUS" = 'text8' ]; then
    if [ "$1" = 'matlab' ]; then
-       matlab -nodisplay -nodesktop -nojvm -nosplash < ./eval/matlab/read_and_evaluate.m 1>&2 
+       matlab -nodisplay -nodesktop -nojvm -nosplash < ./eval/matlab/read_and_evaluate.m 1>&2
    elif [ "$1" = 'octave' ]; then
        octave < ./eval/octave/read_and_evaluate_octave.m 1>&2
    else
